32611a3b861c2555be01ea3b04df93cb8a0f06b4
niconico-arch <71493170+niconico-arch@users.noreply.github.com>
2020-11-04 22:39:19 +0800

    Add files via upload

--- MathLib.cpp
+++ MathLib.cpp
@@ -9,7 +9,7 @@ bool RandGen::seed = false;
 RandGen::RandGen() {
 	/* initialize uniform random number generator */
 	if (!seed) {
-		srand ( (unsigned int)time(NULL) );
+		srand((unsigned int)time(NULL));
 		seed = true;
 	}
 }
@@ -56,26 +56,19 @@ double PolarRej::generate()
 ** DESCRIPTION:	Generates a (double) random number, a sequence of which are
 **				distributed normally with zero mean and unit variance using
 **				Box-Muller transformation (Polar rejection)
-
-** NOTE:		Since each execution of Polar Rejection generates 2 independent normally 
+** NOTE:		Since each execution of Polar Rejection generates 2 independent normally
 **              distributed random numbers with zero mean, unit variance, for efficiency
 **              one random number should be returned, the other one should be saved for
 **              the next call.
 **--------------------------------------------------------------------------*/
 {
 	// declare variables for calculation purpose. For variables to be saved, declare as static variable.
-	double v1, v2, w, n1;
-	static double n2;
-	static int use_last = 0;
+	double v1, v2, w, rv1;
 
-	// if the second random number is not used yet, set the return number to be the second number generated in the last run and set use_last as False
-	if (use_last)
-	{
-		n1 = n2;
-		use_last = 0;
-	}
-	// generate two random numbers following Polar rejection method for Box-Muller transformation
-	else
+	/* if it's the first time running the function (no random variable 2 was saved)
+	** generate two random variables with sin/cos method for Box-Muller transformation
+	** set first as false so that the random number 2 generated can be directly used next time */
+	if (first)
 	{
 		do {
 			v1 = 2.0 * uniform() - 1.0;
@@ -84,12 +77,20 @@ double PolarRej::generate()
 		} while (w >= 1.0);
 
 		w = sqrt((-2.0 * log(w)) / w);
-		n1 = v1 * w;
-		n2 = v2 * w;
-		use_last = 1;
+		rv1 = v1 * w;
+		rv2 = v2 * w;
+		first = false; 
+	}
+	/* if it's not the first time running the function (random variable 2 was saved)
+	** use randome number 2 generate in the previous run directly
+	** set first as true to re-generate two new random number next time*/
+	else
+	{
+		rv1 = rv2;
+		first = true;
 	}
 
-	return(n1);
+	return(rv1);
 }
 
 double SinCos::generate()
@@ -97,55 +98,57 @@ double SinCos::generate()
 ** DESCRIPTION:	Generates a (double) random number, a sequence of which are
 **				distributed normally with zero mean and unit variance using
 **				Box-Muller transformation (sin/cos).
-
 ** NOTE:		Since each execution of SinCos generates 2 independent normally
 **              distributed random numbers with zero mean, unit variance, for efficiency
 **              one random number should be returned, the other one should be saved for
 **              the next call.
 **---------------------------------------------------------------------------*/
 {
-	// declare variables for calculation purpose. For variables to be saved, declare as static variable.
-	double u1, u2, pi, theta, r, n1;
-	static double n2;
-	static int use_last = 0;
+	// declare variables for calculation purpose
+	double u1, u2, pi, theta, r, rv1;
 
-	// if the second random number is not used yet, set the return number to be the second number generated in the last run and set use_last as False
-	if (use_last)
-	{
-		n1 = n2;
-		use_last = 0;
-	}
-	// generate two random numbers following sin/cos method for Box-Muller transformation
-	else
+	/* if it's the first time running the function (no random variable 2 was saved)
+	** generate two random variables with sin/cos method for Box-Muller transformation
+	** set first as false so that the random number 2 generated can be directly used next time */
+	if (first)
 	{
 		u1 = uniform();
 		u2 = uniform();
 		pi = 2 * acos(0.0);
-		theta = 1.0 * pi * u1;
+		theta = 2.0 * pi * u1;
 		r = sqrt(-2.0 * log(u2));
-		n1 = r * cos(theta);
-		n2 = r * sin(theta);
-		use_last = 1;
+		rv1 = r * cos(theta);
+		rv2 = r * sin(theta);
+		first = false;
+	}
+	/* if it's not the first time running the function (random variable 2 was saved)
+	** use randome number 2 generate in the previous run directly
+	** set first as true to re-generate two new random number next time*/
+	else
+	{
+		rv1 = rv2;
+		first = true;
 	}
 
-	return(n1);
+	return(rv1);
 }
+
 
 double Uniform::generate()
 {
 	double rv;
 	do {
-		rv = 1.0*rand()/RAND_MAX;
+		rv = 1.0 * rand() / RAND_MAX;
 	} while (rv <= 0.0 || rv >= 1.0);
 	return rv;
 }
 
 double Exponential::generate()
 {
-	return -log(1.0*rand()/RAND_MAX);
+	return -log(1.0 * rand() / RAND_MAX);
 }
 
 double __stdcall uniform() { return Uniform::Instance()->generate(); }
 double __stdcall exponential() { return Exponential::Instance()->generate(); }
 double __stdcall normalPR() { return PolarRej::Instance()->generate(); }
-double __stdcall normalSC()  { return SinCos::Instance()->generate(); }
+double __stdcall normalSC() { return SinCos::Instance()->generate(); }
