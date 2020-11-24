6511e72c58b6d690a7ab58c980472fb75e8a5147
niconico-arch <71493170+niconico-arch@users.noreply.github.com>
2020-11-24 23:09:33 +0800

    Add files via upload

---
+++ Calendar.cpp
@@ -0,0 +1,65 @@
+#include "Calendar.h"
+#include <cstring>
+#include <cstdlib>
+
+#define maxLineSize	80
+
+bool
+Calendar::roll(date& dt)
+{
+	// TODO
+	return SUCCESS;
+}
+
+bool
+Calendar::isBusDay(const date& dt)
+{
+	// TODO
+	return true;
+}
+
+bool
+Calendar::addBusDays(date& dt, int count)
+{
+	// TODO
+	return SUCCESS;
+}
+
+bool
+Calendar::addMonths(date& dt, int count)
+{
+	// TODO
+	return SUCCESS;
+}
+
+bool
+Calendar::addYears(date& dt, int count)
+{
+	// TODO
+	return SUCCESS;
+}
+
+bool
+Calendar::addHoliday(date& dt)
+{
+	// TODO
+	return SUCCESS;
+}
+
+bool
+Calendar::removeHoliday(date& dt)
+{
+	// TODO
+	return SUCCESS;
+}
+
+MMCalendar::MMCalendar(string filename, string mkt) :market(mkt) {
+	// TODO
+}
+
+bool
+MMCalendar::roll(date& dt)
+{
+	// TODO
+	return SUCCESS;
+}
