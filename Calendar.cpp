ce3bd25553eae69ba4e7af374c8d78557a6cef2b
niconico-arch <71493170+niconico-arch@users.noreply.github.com>
2020-11-25 22:52:16 +0800

    Add files via upload

--- Calendar.cpp
+++ Calendar.cpp
@@ -1,13 +1,18 @@
 #include "Calendar.h"
 #include <cstring>
 #include <cstdlib>
+#include <sstream>
 
 #define maxLineSize	80
 
 bool
 Calendar::roll(date& dt)
+/*-------------------------------------------------------------------
+** DESCRIPTION:	Roll a date forward until business day.
+**
+** RETURNS:		SUCCESS.
+**-----------------------------------------------------------------*/
 {
-	// TODO
 	while (!(isBusDay(dt)))
 	{
 		dt++;
@@ -17,10 +22,16 @@ Calendar::roll(date& dt)
 
 bool
 Calendar::isBusDay(const date& dt)
+/*-------------------------------------------------------------------
+** DESCRIPTION:	Check whether a date is a business day.
+**
+** RETURNS:		TRUE if the date is a business day, else FALSE.
+**-----------------------------------------------------------------*/
 {
-	// TODO
+	/* check whether the date falls into weekend */
 	if (dt.isWeekDay() == false)
 		return false;
+	/* check whether the date falls into any holiday according to the holiday set in the calendar */
 	if (this->holidays.count(dt))
 		return false;
 	return true;
@@ -28,15 +39,25 @@ Calendar::isBusDay(const date& dt)
 
 bool
 Calendar::addBusDays(date& dt, int count)
+/*-------------------------------------------------------------------
+** DESCRIPTION:	add count number of days and roll to a business day.
+**
+** RETURNS:		SUCCESS after adding and rolling else FAILURE
+**-----------------------------------------------------------------*/
 {
-	// TODO
+	dt += count;
+	roll(dt);
 	return SUCCESS;
 }
 
 bool
 Calendar::addMonths(date& dt, int count)
+/*-------------------------------------------------------------------
+** DESCRIPTION:	add count number of months and roll to a business day.
+**
+** RETURNS:		SUCCESS after adding and rolling else FAILURE
+**-----------------------------------------------------------------*/
 {
-	// TODO
 	dt.addMonths(count);
 	roll(dt);
 	return SUCCESS;
@@ -44,8 +65,12 @@ Calendar::addMonths(date& dt, int count)
 
 bool
 Calendar::addYears(date& dt, int count)
+/*-------------------------------------------------------------------
+** DESCRIPTION:	add count number of years and roll to a business day.
+**
+** RETURNS:		SUCCESS after adding and rolling else FAILURE
+**-----------------------------------------------------------------*/
 {
-	// TODO
 	dt.addYears(count);
 	roll(dt);
 	return SUCCESS;
@@ -53,37 +78,62 @@ Calendar::addYears(date& dt, int count)
 
 bool
 Calendar::addHoliday(date& dt)
+/*-------------------------------------------------------------------
+** DESCRIPTION:	add a date into the calendar holiday set.
+**
+** RETURNS:		SUCCESS if added successfully else FAILURE.
+**-----------------------------------------------------------------*/
 {
-	// TODO
-	if (isBusDay(dt))
+	if (!isBusDay(dt))
 	{
-		this->holidays.insert(dt);
+		return FAILURE;
 	}
+	this->holidays.insert(dt);
 	return SUCCESS;
 }
 
 bool
 Calendar::removeHoliday(date& dt)
+/*-------------------------------------------------------------------
+** DESCRIPTION:	Remove a date from the calendar holiday set.
+**
+** RETURNS:		SUCCESS if removed successfully else FAILURE.
+**-----------------------------------------------------------------*/
 {
-	// TODO
+	if (isBusDay(dt))
+	{
+		return FAILURE;
+	}
 	this->holidays.erase(dt);
 	return SUCCESS;
 }
 
-MMCalendar::MMCalendar(string filename, string mkt) :market(mkt) {
-	// TODO
-	string s;
-	const char* split = ",";
-	ifstream holidayFile;
+MMCalendar::MMCalendar(string filename, string mkt) :market(mkt) 
+/*-------------------------------------------------------------------
+** DESCRIPTION:	Constructor for a Money Market calendar by loading 
+**				holidays from a text file.
+**-----------------------------------------------------------------*/ 
+{
+	ifstream holidayFile; // file stream holding the holidays
+	string s; // string to store each line read from the holiday file
 	holidayFile.open(filename);
+
+	/* read the file line by line and add corresponding market holidays into the calendar*/
 	while (getline(holidayFile, s))
 	{
-		char* s_input = (char*)s.c_str();
-		char* p = strtok(s_input, split);
-		date& inputHoliday = date();
-		string inputMarket = ;
+		/* for each line in the file delimited by comma, read the first item as date and second item as market */
+		string dt, inputMarket;
+		std::stringstream ss(s);
+		getline(ss, dt, ',');
+		getline(ss, inputMarket, ',');
+		/* check if market of the line matches the calendar market, add the date into the calendar holiday*/
 		if (inputMarket == mkt)
 		{
+			int year, month, day;
+			year = stoi(dt.substr(0, 4));
+			month = stoi(dt.substr(4, 2));
+			day = stoi(dt.substr(6, 2));
+			date inputHoliday(year, month, day);
 			this->holidays.insert(inputHoliday);
 		}
 	}
@@ -92,20 +142,29 @@ MMCalendar::MMCalendar(string filename, string mkt) :market(mkt) {
 
 bool
 MMCalendar::roll(date& dt)
+/*--------------------------------------------------------------------------
+** DESCRIPTION:	Roll a date to business day in modified following manner.
+**              roll the date forward to a business day if it's not month-end,
+**				otherwise roll it backward to a business day.
+**
+** RETURNS:		SUCCESS after rolled the date successfully
+**-----------------------------------------------------------------*/
 {
-	// TODO
-	date& targetDt = dt;
-	while (!isBusDay(dt))
+	date orgDt = dt; //variable to store the value of the original date
+	/* roll the date forward until a business day */
+	while (!this->isBusDay(dt))
 	{
 		++dt;
 	}
-	if (targetDt.month()!=dt.month())
+	/* if the rolled date ended in a different month, i.e. crossed a month-end
+	** set the date back to the original date and roll backward till a business day*/
+	if (orgDt.month()!=dt.month())
 	{
-		dt = targetDt;
+		dt = orgDt;
 		do 
 		{
 			--dt;
-		} while (!isBusDay(dt));
+		} while (!this->isBusDay(dt));
 	}
 	return SUCCESS;
 }
